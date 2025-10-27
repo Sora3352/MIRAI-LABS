48f67a76c742a2f697f2b20cee40a2727efd4665
Sora3352 <2401003@s.asojuku.ac.jp>
2025-10-27 12:02:16 +0900

    changeFile

--- src/partials/footer.php
+++
@@ -1,8 +0,0 @@
-</main>
-  <footer class="site-footer" role="contentinfo">
-    <div class="footer-inner">
-      <small>&copy; <?php echo date('Y'); ?> MIRAI LABS</small>
-    </div>
-  </footer>
-</body>
-</html>
